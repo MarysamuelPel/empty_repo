c8adaff6727b6504e0dfda887ddb95255e5798f6
Mary <samuelmary1617@gmail.com>
2024-05-06 19:01:52 +0100

    added my links to the project

--- src/index.js
+++ src/index.js
@@ -1,53 +1,53 @@
 
 
-function formatDate(date) {
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
-  let day = date.getDay();
+// function formatDate(date) {
+//   let minutes = date.getMinutes();
+//   let hours = date.getHours();
+//   let day = date.getDay();
 
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+//   if (minutes < 10) {
+//     minutes = `0${minutes}`;
+//   }
 
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
+//   if (hours < 10) {
+//     hours = `0${hours}`;
+//   }
 
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
+//   let days = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday"
+//   ];
 
-  let formattedDay = days[day];
-  return `${formattedDay} ${hours}:${minutes}`;
-}
+//   let formattedDay = days[day];
+//   return `${formattedDay} ${hours}:${minutes}`;
+// }
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+// let searchForm = document.querySelector("#search-form");
+// searchForm.addEventListener("submit", search);
 
-let currentDateELement = document.querySelector("#current-date");
-let currentDate = new Date();
+// let currentDateELement = document.querySelector("#current-date");
+// let currentDate = new Date();
 
-currentDateELement.innerHTML = formatDate(currentDate);
+// currentDateELement.innerHTML = formatDate(currentDate);
 
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#current-temperature-value");
-  let temperature = Math.round(response.data.temperature.current);
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = temperature;
-}
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let city = searchInputElement.value;
-  let apiKey = "bbf4bdf8f1a79ed3fa97o6t0b5932918";
-  let apiUrl = "https://api.shecodes.io/weather/v1/current?query=".concat(city, "&key=").concat(apiKey, "&units=metric");
-  axios.get(apiUrl).then(displayTemperature);
-}
+// function displayTemperature(response) {
+//   let temperatureElement = document.querySelector("#current-temperature-value");
+//   let temperature = Math.round(response.data.temperature.current);
+//   let cityElement = document.querySelector("#current-city");
+//   cityElement.innerHTML = response.data.city;
+//   temperatureElement.innerHTML = temperature;
+// }
+// function search(event) {
+//   event.preventDefault();
+//   let searchInputElement = document.querySelector("#search-input");
+//   let city = searchInputElement.value;
+//   let apiKey = "bbf4bdf8f1a79ed3fa97o6t0b5932918";
+//   let apiUrl = "https://api.shecodes.io/weather/v1/current?query=".concat(city, "&key=").concat(apiKey, "&units=metric");
+//   axios.get(apiUrl).then(displayTemperature);
+// }
 
